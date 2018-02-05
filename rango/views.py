643f9456bdbcf263442313ceae9bdc8e846c0307
Muhammad Ikhwani <ikhwani.muhammad@gmail.com>
2018-02-05 18:10:27 +0000

    chapter 10

--- rango/views.py
+++ rango/views.py
@@ -24,17 +24,14 @@ def index(request):
     response = render ( request, 'rango/index.html', context=context_dict )
     return response
 
-
-
 def about(request):
-    if request.session.test_cookie_worked (): print
-    ( "TEST COOKIE WORKED!" )
-    request.session.delete_test_cookie ()
-    # prints out whether the method is a GET or a POST
-    print(request.method)
-    # prints out the user name, if no one is logged in it prints `AnonymousUser`
-    print(request.user)
-    return render(request, 'rango/about.html', {})
+    return render(request, 'rango/about.html',{})
+    request.session.test_cookie_worked ()
+    context_dict = {'boldmessage': "Yeah, ok"}
+    visitor_cookie_handler ( request )
+    context_dict['visits'] = request.session['visits']
+    response = render ( request, 'rango/about.html', context=context_dict )
+    return response
 
 def show_category(request, category_name_slug):
     context_dict = {}
